0c5d155a78ce7076606bb25ccfd5d283683259f4
Matsuda Kenta <kenta.matsuda1125@gmail.com>
2024-02-21 22:33:24 +0900

    train.csvの前処理部分を追加

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 # パッケージのインポート
 import pandas as pd
+import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
@@ -7,16 +8,16 @@ from nltk.stem import WordNetLemmatizer
 # csvの相対パス
 path = './data/train.csv'
 data = pd.read_csv(path)
+data['title'] = data['title'].astype(str)
+data['abstract'] = data['abstract'].astype(str)
 
-# データの確認
-print(data.head)
 
 # テキストの前処理関数を定義する
 def preprocess_text(text):
     # 小文字化
     text = text.lower()
     # 特殊文字の除去
-    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = re.sub(r'[^a-zA-Z\s]', '', text) 
     # 単語のトークン化
     tokens = word_tokenize(text)
     # ストップワードの除去
@@ -29,9 +30,10 @@ def preprocess_text(text):
     text = ' '.join(tokens)
     return text
 
+
 # タイトルとアブストラクトの列に前処理を適用する
-data['clean_title'] = data['Title'].apply(preprocess_text)
-data['clean_abstract'] = data['Abstract'].apply(preprocess_text)
+data['clean_title'] = data['title'].apply(preprocess_text)
+data['clean_abstract'] = data['abstract'].apply(preprocess_text)
 
 # 前処理済みデータの最初の数行を表示して確認する
 print(data[['clean_title', 'clean_abstract']].head())
